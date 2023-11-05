dffe5c6423cdc11d2a75ac8747fe16d4e659a980
ahmet422 <ahmet.tajik@icloud.com>
2023-11-05 15:44:27 -0600

    added some other properties.

--- src/main/java/com/example/springboot/demo/myCoolApp/rest/FunRestController.java
+++ src/main/java/com/example/springboot/demo/myCoolApp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.example.springboot.demo.myCoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose team info
+    @GetMapping("/teamInfo")
+    public String teamIfo(){
+        return "Coach name: " + coachName + " and team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
